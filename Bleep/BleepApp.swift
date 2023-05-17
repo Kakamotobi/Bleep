5128034b162ccf1a522e24961d1f4b0fe7714b0b
Daeram Chung <daeram.chung@gmail.com>
2023-05-17 19:20:20 +0900

    Initial Commit

---
+++ Bleep/BleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleepApp.swift
+//  Bleep
+//
+//  Created by Daeram Chung on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct BleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
