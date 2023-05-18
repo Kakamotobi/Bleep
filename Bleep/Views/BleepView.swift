159dea52ee4bfb5da509977ca468566004690e65
Daeram Chung <daeram.chung@gmail.com>
2023-05-18 16:10:35 +0900

    Format bleep display
    - Account for mins or hours.

--- Bleep/Views/BleepView.swift
+++ Bleep/Views/BleepView.swift
@@ -11,8 +11,15 @@ struct BleepView: View {
     @ObservedObject var bleep: Bleep
     
     var body: some View {
+        let (hours, mins) = convertSecsToMinsHours(bleep.intervalInSeconds)
+        let bleepText = """
+            \(bleep.content) - every \(hours >= 1
+                ? "\(hours.formatted()) hour\(hours > 1 ? "s" : "")"
+                : "\(mins.formatted()) min\(mins > 1 ? "s" : "")")
+        """
+        
         HStack{
-            Text("\(bleep.content) - every \(bleep.interval.formatted()) mins")
+            Text("\(bleepText)")
             Spacer()
             Toggle("", isOn: $bleep.isActive)
                 .toggleStyle(.switch)
--- Bleep/Views/BleepsListView.swift
+++ Bleep/Views/BleepsListView.swift
@@ -15,13 +15,11 @@ struct BleepsListView: View {
             ForEach(modelData.bleeps) {bleep in
                 BleepView(bleep: bleep)
             }
-        }.border(.green)
+        }
     }
 }
 
 struct BleepsList_Previews: PreviewProvider {
-    @State static var bleeps = [Bleep(content: "stand up", interval: 2)]
-    
     static var previews: some View {
         BleepsListView()
             .environmentObject(ModelData())
